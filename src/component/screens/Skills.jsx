441e38cb600481925e7a9018bddb0fde75a25f33
CodesOfVysakh <onvysakh309@gmail.com>
2023-05-23 16:18:51 +0530

    skills button like format

--- src/component/screens/Skills.jsx
+++ src/component/screens/Skills.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 function Skills() {
     return (
-        <section id="skill-section" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 h-screen">
-            skills
+        <section id="skill-section" className="mx-auto max-w-7xl p-6 lg:px-8 h-screen">
+            <div className="head-title">
+                <h3 className="text-center text-7xl font-semibold mb-9">Skills</h3>
+            </div>
+            <div className="content-box flex items-center justify-between">
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>React Js</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>HTML5</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>CSS3</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>Javascript</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>Python Django</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>React Native</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>Git</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>REST Api</span>
+            </div>
         </section>
     )
 }
