26d46a1a852fa178fbe8de6d657724d6a6e66112
CodesOfVysakh <onvysakh309@gmail.com>
2023-05-26 17:47:22 +0530

    skills wrapped and justified

--- src/component/screens/Skills.jsx
+++ src/component/screens/Skills.jsx
@@ -6,17 +6,17 @@ function Skills() {
             <div className="head-title">
                 <h3 className="text-center text-7xl font-semibold mb-9">Skills</h3>
             </div>
-            <div className="content-box flex items-center justify-between">
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>React Js</span>
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>HTML5</span>
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>CSS3</span>
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>Javascript</span>
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>Python Django</span>
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>React Native</span>
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>Git</span>
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>REST Api</span>
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>Tailwind</span>
-                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6'>React BootStrap</span>
+            <div className="content-box flex items-center justify-center flex-wrap gap-5">
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>React Js</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>HTML5</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>CSS3</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>Javascript</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>Python Django</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>React Native</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>Git</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>REST Api</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>Tailwind</span>
+                <span className='rounded-full bg-fuchsia-900 shadow-lg shadow-green-500/20  py-4 px-6 w-fit'>React BootStrap</span>
             </div>
         </section>
     )
